8028d2170a2407fd0f370ab06c14d0a44e0e253a
Chris <cjriddle31@gmail.com>
2025-06-25 18:00:06 -0700

    replace bookmark plugin with bunny. setup shortcut paths

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,2 +1,39 @@
 require("git"):setup()
 require("simple-status"):setup()
+
+-- Bookmarks
+require("bunny"):setup({
+    hops = {
+        -- Home
+        { key='h', path='~/' },
+        { key='a', path='~/Audio' },
+        { key='A', path='~/App' },
+        { key='c', path='~/Code' },
+        { key='d', path='~/Docs' },
+        { key='g', path='~/Game' },
+        { key='i', path='~/Img' },
+        { key='m', path='~/Music' },
+        { key='s', path='~/Sync' },
+        { key='t', path='~/Temp' },
+
+        -- Dots
+        { key={'.','c'}, path='~/.config' },
+        { key={'.','b'}, path='~/.local/bin' },
+        { key={'.','l'}, path='~/.local/' },
+        { key={'.','s'}, path='~/.local/share' },
+
+        -- Root
+        { key={'/','r'}, path='/' },
+        { key={'/','e'}, path='/etc' },
+        { key={'/','m'}, path='/media' },
+        { key={'/','o'}, path='/opt' },
+        { key={'/','s'}, path='/usr/share' },
+        { key={'/','u'}, path='/usr' },
+        { key={'/','v'}, path='/var' },
+    },
+    desc_strategy = "path",
+    ephemeral = true,
+    tabs = true,
+    notify = false,
+    fuzzy_cmd = "fzf"
+})
