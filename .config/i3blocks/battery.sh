5b6f870fa83a1572fb4ac3739f06159e5b15a804
Chris <cjriddle31@gmail.com>
2025-07-21 15:31:09 -0700

    improved battery script for i3blocks

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-BAT1=$(acpi -b | grep -E -o '[0-9][0-9]' | sed -n '1 p')
-BAT2=$(acpi -b | grep -E -o '[0-9][0-9]' | sed -n '2 p')
-BAT_TOT=$(($BAT1+$BAT2))
+# Get battery status
+BAT1=$(acpi -b | grep -E -o '([0-9]+)%' | sed -n '1 p')
+BAT1I=$(echo $BAT1 | grep -E -o '([0-9]+)')
 
-# Full and short texts
-echo "⚡BAT: $BAT1% + $BAT2%"
-#echo $BAT_TOT
+BAT2=$(acpi -b | grep -E -o '([0-9]+)%' | sed -n '2 p')
+BAT2I=$(echo $BAT2 | grep -E -o '([0-9]+)')
+
+BAT_TOT=$(($BAT1I + $BAT2I))
+BAT_WARN=$(($BAT_TOT / 8))
+BAT_CRIT=$(($BAT_TOT / 15))
+
+# Check charging status / set icons
+IS_CHARGING=$(acpi -b | grep -E -o 'Charging')
+
+if [ "$IS_CHARGING" == "Charging" ]; then
+    ICON="⚡"
+else
+    ICON="🔋"
+fi
 
 # Set urgent flag below 5% or use orange below 20%
-[ ${BAT_TOT%?} -le 5 ] && exit 33
-[ ${BAT_TOT%?} -le 20 ] && echo "#FF8000"
+if [ ${BAT_TOT} -le ${BAT_WARN} ]; then
+    ICON="🪫"
+    BG_COLOR="#FF8000"
+fi
+
+# Set status text
+echo "$ICON bat: $BAT1 + $BAT2 $BG_COLOR"
+
+# ?
+if [ ${BAT_TOT%?} -le ${BAT_CRIT} ]; then 
+    exit 33
+fi
 
 exit 0
