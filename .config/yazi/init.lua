b35429a0f7b38aae4c960fdd342e251f34041bfa
CJ <cjriddle31@gmail.com>
2026-05-08 12:02:20 -0700

    yazi update

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -7,14 +7,19 @@ th.git.deleted_sign = "D"
 th.git.updated_sign = "U"
 
 require("git"):setup{
-    -- Order of status signs showing in the linemode
     order = 0,
 }
 
-require("simple-status"):setup()
+Status:children_add(function()
+    local h = cx.active.current.hovered
+    if not h or ya.target_family() ~= "unix" then
+        return ""
+    end
 
---require("bookmarks"):setup({
---    last_directory = { enable = true, persist = false, mode="dir" },
---    persist = "all",
---    file_pick_mode = "parent",
---})
+    return ui.Line {
+        ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+        ":",
+        ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+        " ",
+    }
+end, 500, Status.RIGHT)
