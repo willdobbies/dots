3f1b24f086785162cf7977805747e74e64a98db2
criddle <criddle@hrcwater.org>
2026-06-29 15:24:22 -0700

    Update yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -23,3 +23,22 @@ Status:children_add(function()
         " ",
     }
 end, 500, Status.RIGHT)
+
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+    last_directory = { enable = false, persist = false, mode="dir" },
+    persist = "all",
+    desc_format = "full",
+    file_pick_mode = "hover",
+    custom_desc_input = false,
+    show_keys = true,
+    notify = {
+        enable = true,
+        timeout = 1,
+        message = {
+            new = "New bookmark '<key>' -> '<folder>'",
+            delete = "Deleted bookmark in '<key>'",
+            delete_all = "Deleted all bookmarks",
+        },
+    },
+})
